23e24ef613bf27307807bb25bdade8801c6c4061
Hao-yu Liao <jeffery84115@gmail.com>
2021-04-25 22:43:23 +0800

    210425: 2 bugs left

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-import os
+import os, re, json
 
 from flask import *
 from sqlalchemy import create_engine, text
+
 
 app=Flask(__name__)
 app.config["JSON_AS_ASCII"]=False
@@ -10,50 +11,36 @@ app.config["TEMPLATES_AUTO_RELOAD"]=True
 engine = create_engine('mysql+pymysql://root:haoyuliao@localhost/web_tdtw')
 connection = engine.connect()
 execute = connection.execute
-t_trial = text(
-    'select * from attractions limit 5'
+
+t_api_attrs_keyword = text(
+    "select * from attractions like '%':keyword'%' limit :idStart, 12"
+)
+t_api_attrs_noKeyword = text(
+    'select * from attractions limit :idStart, 12'
+)
+t_api_attr_id = text(
+    'select * from attractions where id = :id'
 )
 
-print('trial: ', execute(t_trial).first())
-
-# app.secret_key的目的為何？
-
-'''
-# Roam Research：((gMX8Qu-bZ))
-
-print("os.environ.get('HOME'): ", os.environ.get('HOME'))
-# print("os.environ['MYSQL_PASSWORD']: ")
-# print(os.environ['MYSQL_PASSWORD'])
-MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD')
-print("os.environ.get('MYSQL_PASSWORD'): ")
-print(MYSQL_PASSWORD)
-
-ENGINE_URL = 'mysql+pymysql://root:' + MYSQL_PASSWORD + '@localhost/website'
-print(ENGINE_URL)
-engine = create_engine(ENGINE_URL)
-'''
-
-'''
 # Function Library
-def cleanseImagesData(originStr, ):
-	originStr.replace("\\", "")
-	originStr.replace("http", " http")
-	originStr.replace(" http", "http", 1)
-	listStr = originStr.split()
+def cleanseImagesData(originStr):
+	originStr = originStr.replace("\\", ",")
+	originStr = originStr.replace("http", ",http")
+	originStr = originStr.replace(",http", "http", 1)
+	listStr = originStr.split(",")
+	
 	for str in listStr:
-		if (str.endswith("jpg")) | (str.endswith("png")): # 用萬用字元來寫，大小寫皆可
+		if not (re.match(r".*jpg", str, flags=re.IGNORECASE)):
+			if not (re.match(r".*png", str, flags=re.IGNORECASE)):
+				# print('delete: ', str)
+				listStr.remove(str)
 
-
-
-	# 以http來分割，把str變成List
-	# 若不是jpg, png，就不予加入
-'''
+	# print('listStr: ', listStr)
+	return listStr
 
 # Pages
 @app.route("/")
 def index():
-	# 測試
-	print(execute(t_trial).first())
 	return render_template("index.html")
 @app.route("/attraction/<id>")
 def attraction(id):
@@ -65,80 +52,109 @@ def booking():
 def thankyou():
 	return render_template("thankyou.html")
 
-'''
 @app.route("/api/attractions", methods=["GET"])
 def api_attractions():
-	try:
-		page = request.args.get('page')
+	# try:
+		page = int(request.args.get('page'))
 		keyword = request.args.get('keyword')
-		if (bool(page) & (page >= 0)):
-			idStart = (page * 12 ) + 1
-			dataReturn = {}
-			dataReturn['nextPage'] = page + 1
+		print(page)
+		print(keyword)
 
-			# images要再處理過，可以寫成一個函式來重複使用
+		if page >= 0:
+			idStart = (page * 12 ) + 1
+			dataReturn = {
+				'nextPage': page + 1,
+				'data': []
+			}
+			# print('dataReturn: ', dataReturn)
 
 			def fetchSrcdatas():
 				if bool(keyword):
 					# 如果有keyword
-					srcdatas = select * from attractions where name like %keyword% limit idStart, 12
+					srcdatas = execute(t_api_attrs_keyword, keyword=keyword, idStart=idStart)
+					# srcdatas = where(attractions.c.name.like(’梅%’))
+
 				else:
 					# 如果沒有keyword
-					srcdatas = select * from attractions limit idStart, 12
+					srcdatas = execute(t_api_attrs_noKeyword, idStart=idStart)
 					
 				return srcdatas
-			
+
 			srcdatas = fetchSrcdatas()
+			# print('srcdatas: ', srcdatas)
 
-			id = idStart
+			id = idStart - 1
+			# print('id: ', id)
+			
 			for srcdata in srcdatas:
+
+				srcdata = dict(srcdata)
+				# print('srcdata: ', srcdata)
+				dataToAppend = {}
+
 				for key in srcdata:
-					dataReturn['data'][id][key] = srcdata[key]
+					if key != 'images':
+						dataToAppend[key] = srcdata[key]
+					else:
+						dataToAppend[key] = cleanseImagesData(srcdata[key])
+
 				else:
+					dataReturn['data'].append(dataToAppend)
 					id += 1
-		response.body = json.dump(dataReturn)
-		response.statuscode = 200
 
-		return response
+			# print('dataReturn: ', dataReturn)
 
-	catch:
-		errorReturn = {}
-		errorReturn['error'] = True
-		errorReturn['message'] = "自訂的錯誤訊息"
-		response.body = json.dump(errorReturn)
-		response.statuscode = 500
+		
+		return json.dumps(dataReturn, ensure_ascii=False), 200
 
-		return response
+'''	
+except:
+	errorReturn = {
+		"error": True,
+		"message": "自訂的錯誤訊息"
+	}
+
+	return json.dumps(errorReturn, ensure_ascii=False), 500
+'''
 
 @app.route("/api/attraction/<attractionId>", methods=["GET"])
 def api_attraction(attractionId):
 	try:
-		srcdata = select * from attractions where id = attractionId # 要用.first
-		if (srcdata = None):
-			errorReturn = {}
-			errorReturn['error'] = True
-			errorReturn['message'] = "自訂的錯誤訊息"
-			response.body = json.dump(errorReturn)
-			response.statuscode = 400
+		srcdata = execute(t_api_attr_id, id = attractionId).first()
+		
+		if srcdata == None:
+			errorReturn = {
+				"error": True,
+				"message": "自訂的錯誤訊息"
+			}
+
+			return json.dumps(errorReturn, ensure_ascii=False), 400
 		
 		else:
-			dataReturn = {}
+			srcdata = dict(srcdata)
+			print('srcdata: ', srcdata)
+			dataToAppend = {}
+
 			for key in srcdata:
-				dataReturn['data'][key] = srcdata[key]
+				if key != 'images':
+					dataToAppend[key] = srcdata[key]
+				else:
+					dataToAppend[key] = cleanseImagesData(srcdata[key])
 
-			response.body = json.dump(dataReturn)
-			response.statuscode = 200
+			dataReturn = {
+				'data': dataToAppend
+			}
 
-		return response
+			print('dataReturn: ', dataReturn)
+
+			return json.dumps(dataReturn, ensure_ascii=False), 200
 	
-	catch:
-		errorReturn = {}
-		errorReturn['error'] = True
-		errorReturn['message'] = "自訂的錯誤訊息"
-		response.body = json.dump(errorReturn)
-		response.statuscode = 500
+	except:
+		errorReturn = {
+			"error": True,
+			"message": "自訂的錯誤訊息"
+		}
 
-		return response
-'''
+		return json.dumps(errorReturn, ensure_ascii=False), 500
 
 app.run(host="0.0.0.0", port=3000)
